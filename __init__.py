754cac05490fa8988cb7b9a64f54c85b0754776b
CYX004 <96913880+CYX004@users.noreply.github.com>
2022-01-09 02:05:56 +0800

    update

--- __init__.py
+++ __init__.py
@@ -2,12 +2,13 @@ from flask import Flask, render_template, request, redirect, url_for
 from Forms import CreateSupplierForm, CreateOrderForm
 import shelve, Supplier, Order
 
-
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
 
 @app.route('/createSupplier', methods=['GET', 'POST'])
 def create_supplier():
@@ -18,22 +19,33 @@ def create_supplier():
 
         try:
             suppliers_dict = db['Suppliers']
+            supplier_list = []
+            for key in suppliers_dict:
+                suppliers = suppliers_dict.get(key)
+                supplier_list.append(suppliers)
+            for key in supplier_list:
+                Supplier.Supplier.count_id = key.get_supplier_id()
         except:
             print("Error in retrieving Suppliers from supplier.db.")
 
-        supplier = Supplier.Supplier(create_supplier_form.first_name.data, create_supplier_form.last_name.data, create_supplier_form.gender.data, create_supplier_form.membership.data, create_supplier_form.remarks.data)
+
+        supplier = Supplier.Supplier(create_supplier_form.first_name.data, create_supplier_form.last_name.data,
+                                     create_supplier_form.gender.data, create_supplier_form.membership.data,
+                                     create_supplier_form.remarks.data)
         suppliers_dict[supplier.get_supplier_id()] = supplier
         db['Suppliers'] = suppliers_dict
 
         # Test codes
         suppliers_dict = db['Suppliers']
         supplier = suppliers_dict[supplier.get_supplier_id()]
-        print(supplier.get_first_name(), supplier.get_last_name(), "was stored in supplier.db successfully with supplier_id ==", supplier.get_supplier_id())
+        print(supplier.get_first_name(), supplier.get_last_name(), "was stored in supplier.db successfully with supplier_id ==",
+              supplier.get_supplier_id())
 
         db.close()
 
         return redirect(url_for('retrieve_suppliers'))
     return render_template('createSupplier.html', form=create_supplier_form)
+
 
 @app.route('/createOrder', methods=['GET', 'POST'])
 def create_order():
@@ -44,14 +56,20 @@ def create_order():
 
         try:
             orders_dict = db['Orders']
+            orders_list = []
+            for key in orders_dict:
+                orders = orders_dict.get(key)
+                orders_list.append(orders)
+            for key in orders_list:
+                Order.Order.count_id = key.get_order_id()
         except:
             print("Error in retrieving Orders from order.db.")
 
         order = Order.Order(create_order_form.first_name.data, create_order_form.last_name.data,
-                                     create_order_form.gender.data, create_order_form.membership.data,
-                                     create_order_form.remarks.data, create_order_form.email.data,
-                                     create_order_form.date_joined.data,
-                                     create_order_form.address.data, )
+                            create_order_form.gender.data, create_order_form.membership.data,
+                            create_order_form.remarks.data, create_order_form.email.data,
+                            create_order_form.date_joined.data,
+                            create_order_form.address.data, )
         orders_dict[order.get_order_id()] = order
         db['Orders'] = orders_dict
 
@@ -59,6 +77,7 @@ def create_order():
 
         return redirect(url_for('home'))
     return render_template('createOrder.html', form=create_order_form)
+
 
 @app.route('/retrieveSuppliers')
 def retrieve_suppliers():
@@ -74,6 +93,7 @@ def retrieve_suppliers():
 
     return render_template('retrieveSuppliers.html', count=len(suppliers_list), suppliers_list=suppliers_list)
 
+
 @app.route('/retrieveOrders')
 def retrieve_orders():
     orders_dict = {}
@@ -87,6 +107,7 @@ def retrieve_orders():
         orders_list.append(order)
 
     return render_template('retrieveOrders.html', count=len(orders_list), orders_list=orders_list)
+
 
 @app.route('/updateSupplier/<int:id>/', methods=['GET', 'POST'])
 def update_supplier(id):
@@ -122,10 +143,11 @@ def update_supplier(id):
 
         return render_template('updateSupplier.html', form=update_supplier_form)
 
+
 @app.route('/updateOrder/<int:id>/', methods=['GET', 'POST'])
 def update_order(id):
     update_order_form = CreateOrderForm(request.form)
-    if request.method == 'POST' and update_order_form.validate():
+    if request.method == 'POST':
         orders_dict = {}
         db = shelve.open('order.db', 'w')
         orders_dict = db['Orders']
@@ -158,5 +180,4 @@ def update_order(id):
 
 
 if __name__ == '__main__':
-    app.run()
-
+    app.run(debug=True)
